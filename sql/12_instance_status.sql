19fb3a15b406e6a4aac66eb42131123f9e3f1d3b
Yacine31 <yacine.oumghar@gmail.com>
2023-11-10 22:26:37 +0100

    Update 12_instance_status.sql

--- sql/12_instance_status.sql
+++ sql/12_instance_status.sql
@@ -2,13 +2,21 @@ select
  inst_id,
  instance_name,
  status,
- VERSION_FULL,
- EDITION,
+-- VERSION_FULL,
+-- EDITION,
  ARCHIVER,
  INSTANCE_ROLE,
  database_status
 FROM  gv$instance;
 
-SELECT inst_id, name, to_char(CREATED ,'DD/MM/YYYY') CREATED , open_mode, DATABASE_ROLE, log_mode, FORCE_LOGGING, CURRENT_SCN FROM gv$database;
+SELECT 
+ inst_id,
+ name,
+ to_char(CREATED ,'DD/MM/YYYY') CREATED ,
+ open_mode,
+ DATABASE_ROLE,
+ log_mode,
+ FORCE_LOGGING,
+ CURRENT_SCN FROM gv$database;
 
 exit
