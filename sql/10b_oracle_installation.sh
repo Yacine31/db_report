1058038876d01a3e0a7bfe5649d9e8d91002b2ab
Yacine31 <yacine.oumghar@gmail.com>
2023-11-12 03:59:52 +0100

    Update 10b_oracle_installation.sh

--- sql/10b_oracle_installation.sh
+++ sql/10b_oracle_installation.sh
@@ -1,11 +1,11 @@
 # inventory.xml
 ORA_INVENTORY=$(cat /etc/oraInst.loc | grep inventory_loc | cut -d= -f2)
-cat $ORA_INVENTORY/ContentsXML/inventory.xml >> ${HTML_FILE}
+cat $ORA_INVENTORY/ContentsXML/inventory.xml
 
 # opatch 
 ORA_HOME=$(cat /etc/oratab | egrep -v "^$|^#" | cut -d: -f2 | sort -u)
 for OH in ${ORA_HOME}
 do 
-	export ORACLE_HOME=${ORA_HOME}; ${ORA_HOME}/OPatch/opatch lspatches >> ${HTML_FILE}
+	export ORACLE_HOME=${ORA_HOME}; ${ORA_HOME}/OPatch/opatch lspatches 
 done
 
