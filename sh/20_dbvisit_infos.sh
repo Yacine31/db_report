8fd6b4bb7b1f0144a0c494bad750f4c5c15ca8e5
Yacine31 <yacine.oumghar@gmail.com>
2023-11-12 05:24:32 +0100

    Update 20_dbvisit_infos.sh

--- sh/20_dbvisit_infos.sh
+++ sh/20_dbvisit_infos.sh
@@ -8,11 +8,16 @@ echo "<h2>dbvctl gap report</h2>"
 # export DBV_HOME=/usr/dbvisit/standbymp/oracle
 export DBV_HOME=$(dirname $(ps -ef | grep dbvctl | grep -v grep | awk '{print $8}'))
 
-cd ${DBV_HOME}/conf 
-ls -1 dbv_*.env | sed 's/dbv_//g' | sed 's/.env//g' | while read db
-do
-	echo "<pre>"
-	${DBV_HOME}/dbvctl -d $db -i
-	echo "</pre>"
-	echo "<br>"
-done
+echo "<pre>"
+${DBV_HOME}/dbvctl -d ${ORACLE_SID} -i
+echo "</pre>"
+
+
+# cd ${DBV_HOME}/conf 
+# ls -1 dbv_*.env | sed 's/dbv_//g' | sed 's/.env//g' | while read db
+# do
+#	echo "<pre>"
+#	${DBV_HOME}/dbvctl -d $db -i
+#	echo "</pre>"
+#	echo "<br>"
+# done
