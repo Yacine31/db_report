818fe615aa66e06b8bf3bc3b000ac9618446d1db
Yacine31 <yacine.oumghar@gmail.com>
2023-11-12 05:30:37 +0100

    Update 20_dbvisit_infos.sh

--- sh/20_dbvisit_infos.sh
+++ sh/20_dbvisit_infos.sh
@@ -1,23 +1,29 @@
-# dbvisit
-echo "<h2>Process DBVisit</h2>"
-echo "<pre>"
-ps -ef | grep dbvctl | grep -v grep 
-echo "</pre>"
+# run script only if dbvctl is running
 
-echo "<h2>dbvctl gap report</h2>"
-# export DBV_HOME=/usr/dbvisit/standbymp/oracle
-export DBV_HOME=$(dirname $(ps -ef | grep dbvctl | grep -v grep | awk '{print $8}'))
+count=$(ps -ef | grep dbvctl | grep -v grep | wc -l)
 
-echo "<pre>"
-${DBV_HOME}/dbvctl -d ${ORACLE_SID} -i
-echo "</pre>"
+if [ $count -gt 0 ]; then
+	# dbvisit
+	echo "<h2>Process DBVisit</h2>"
+	echo "<pre>"
+	ps -ef | grep dbvctl | grep -v grep 
+	echo "</pre>"
+
+	echo "<h2>dbvctl gap report</h2>"
+	# export DBV_HOME=/usr/dbvisit/standbymp/oracle
+	export DBV_HOME=$(dirname $(ps -ef | grep dbvctl | grep -v grep | awk '{print $8}'))
+
+	echo "<pre>"
+	${DBV_HOME}/dbvctl -d ${ORACLE_SID} -i
+	echo "</pre>"
 
 
-# cd ${DBV_HOME}/conf 
-# ls -1 dbv_*.env | sed 's/dbv_//g' | sed 's/.env//g' | while read db
-# do
-#	echo "<pre>"
-#	${DBV_HOME}/dbvctl -d $db -i
-#	echo "</pre>"
-#	echo "<br>"
-# done
+	# cd ${DBV_HOME}/conf 
+	# ls -1 dbv_*.env | sed 's/dbv_//g' | sed 's/.env//g' | while read db
+	# do
+	#	echo "<pre>"
+	#	${DBV_HOME}/dbvctl -d $db -i
+	#	echo "</pre>"
+	#	echo "<br>"
+	# done
+fi
