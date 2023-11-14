8984fb794b888d1b9c7dc74c82e5762a86d0922b
Yacine31 <yacine.oumghar@gmail.com>
2023-11-14 02:41:18 +0100

    Update 09_os_info.sh

--- sh/09_os_info.sh
+++ sh/09_os_info.sh
@@ -1,39 +1,39 @@
-echo "<h2>Running Databases</h2>"
+echo "<h2>Bases de données en cours d'exécution :</h2>"
 echo "<pre>"
 ps -ef | grep pmon | grep -v grep 
 echo "</pre>"
 
-echo "<h2>Running listeners</h2>"
+echo "<h2>Listeners en cours d'exécution :</h2>"
 echo "<pre>"
 ps -ef | grep tnslsnr| grep -v grep 
 echo "</pre>"
 
-echo "<h2>Disk Size df -h</h2>"
+echo "<h2>Espace disque (df -h) :</h2>"
 echo "<pre>"
 df -h
 echo "</pre>"
 
-echo "<h2>lsblk -f </h2>"
+echo "<h2>Liste des disques disponibles (lsblk -f) :</h2>"
 echo "<pre>"
 lsblk -f
 echo "</pre>"
 
-echo "<h2>cat /etc/fstab </h2>"
+echo "<h2>Contenu du fichier /etc/fstab :</h2>"
 echo "<pre>"
 cat /etc/fstab | egrep -v '^#|^$'
 echo "</pre>"
 
-echo "<h2>Memory Size (Mo) : free -m </h2>"
+echo "<h2>Taille mémoire en Mo (free -m) :</h2>"
 echo "<pre>"
 free -m
 echo "</pre>"
 
-echo "<h2>lscpu </h2>"
+echo "<h2>Caractéristiques CPU (lscpu) :</h2>"
 echo "<pre>"
 lscpu
 echo "</pre>"
 
-echo "<h2>ulimit -a </h2>"
+echo "<h2>Limites de l'utilisateur "oracle" (ulimit -a) :</h2>"
 echo "<pre>"
 ulimit -a | sort
 echo "</pre>"
