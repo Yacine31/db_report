f58ccde28dbd206634c7628b40180d75d38a9cc4
Yacine31 <yacine.oumghar@gmail.com>
2023-11-14 03:10:09 +0100

    Update 16_dba_registry.sql

--- sql/16_dba_registry.sql
+++ sql/16_dba_registry.sql
@@ -1,10 +1,17 @@
 prompt <h2>Fonctionnalités installées dans la base de données (DBA_REGISTRY) :</h2>
-SELECT /*+  NO_MERGE  */ 
-       x.*
-	   --,c.name con_name
+SELECT  
+       COMP_ID,
+       COMP_NAME,
+       VERSION,
+       VERSION_FULL,
+       STATUS,
+       MODIFIED,
+       SCHEMA,
+       PROCEDURE
   FROM dba_registry x
-       --LEFT OUTER JOIN v$containers c ON c.con_id = x.con_id
 ORDER BY
-       --x.con_id,
 	   x.comp_id;
 exit;
+
+
+
