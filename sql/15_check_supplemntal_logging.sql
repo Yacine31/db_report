282ff3a4c29ab30248ae003b62d5078d8e207cf7
Yacine31 <yacine.oumghar@gmail.com>
2023-11-14 03:02:13 +0100

    Update 15_check_supplemntal_logging.sql

--- sql/15_check_supplemntal_logging.sql
+++ sql/15_check_supplemntal_logging.sql
@@ -1,4 +1,12 @@
-prompt <h2>Database supplemental logging </h2>
+prompt <h2>Database supplemental logging :</h2>
+
+col data_min for a10
+col data_pk for a10
+col data_ui for a10
+col data_fk for a10
+col data_all for a10
+col data_pl for a10
+
 SELECT 
 	supplemental_log_data_min data_min, 
 	supplemental_log_data_pk data_pk, 
