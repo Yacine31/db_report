9889050e0db17d04b977b693136f6ef256bb5abc
Yacine31 <yacine.oumghar@gmail.com>
2023-11-14 02:44:10 +0100

    Update 10_oracle_installation.sh

--- sh/10_oracle_installation.sh
+++ sh/10_oracle_installation.sh
@@ -1,18 +1,18 @@
 # /etc/oratab
-echo "<h2>/etc/oratab</h2>"
+echo "<h2>Contenu du fichier /etc/oratab :</h2>"
 echo "<pre>"
 cat /etc/oratab | egrep -v "^$|^#"
 echo "</pre>"
 
 # inventory.xml
-echo "<h2>Inventory.xml</h2>"
+echo "<h2>Contenu du fichier Inventory.xml :</h2>"
 echo "<pre>"
 ORA_INVENTORY=$(cat /etc/oraInst.loc | grep inventory_loc | cut -d= -f2)
 cat $ORA_INVENTORY/ContentsXML/inventory.xml | grep "<HOME NAME=" | awk '{print $2 " " $3}'
 echo "</pre>"
 
 # opatch 
-echo "<h2>Opatch lspatches</h2>"
+echo "<h2>Niveau de patch des ORACLE_HOME (opatch lspatches) :</h2>"
 cat /etc/oratab | egrep -v "^$|^#" | cut -d: -f2 | sort -u | while read oh
 do 
 	echo "<pre>"
