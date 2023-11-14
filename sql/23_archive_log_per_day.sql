52fa23dfffed1d68a13f235f9016d9bfe077039b
Yacine31 <yacine.oumghar@gmail.com>
2023-11-14 13:22:04 +0100

    Update 23_archive_log_per_day.sql

--- sql/23_archive_log_per_day.sql
+++ sql/23_archive_log_per_day.sql
@@ -66,9 +66,10 @@ order by to_char(first_time, 'YYYY/MM/dd')
 prompt <h2>Taille des redolog par jour :</h2>
 select
         to_char(first_time, 'YYYY/MM/dd') "Jour",
-    count(*) "Nbr de fichiers",
+        count(*) "Nbr de fichiers",
         ROUND(sum(BLOCKS*BLOCK_SIZE)/1024/1024, 0) "Taille_Mo"
 from v$archived_log
+where rownum<=30
 group by to_char(first_time, 'YYYY/MM/dd')
 order by to_char(first_time, 'YYYY/MM/dd')
 ;
