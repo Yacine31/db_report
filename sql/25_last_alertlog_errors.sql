892c355106b0ae9002671b3d1380e782deb7cfa4
Yacine31 <yacine.oumghar@gmail.com>
2023-11-15 12:27:04 +0100

    Update 25_last_alertlog_errors.sql

--- sql/25_last_alertlog_errors.sql
+++ sql/25_last_alertlog_errors.sql
@@ -1,10 +1,9 @@
 prompt <h2>Les dernières erreurs de la base de données (Les 30 derniers jours et les 100 dernières lignes)</h2>
 
 set pages 99
--- select to_char(ORIGINATING_TIMESTAMP, 'DD-MM-YYYY HH-MM-SS') || ' : ' || message_text "Last alertlog (30 days)"
 select to_char(ORIGINATING_TIMESTAMP, 'DD-MM-YYYY HH-MM-SS') "Date", message_text "Message"
 FROM X$DBGALERTEXT
 WHERE originating_timestamp > systimestamp - 30  
 AND regexp_like(message_text, '(ORA-)')
 AND rownum <=100;
-exit
+
