3abbfcea801f406d3336db515c7f874a6c8494bf
Yacine31 <yacine.oumghar@gmail.com>
2023-11-15 12:05:39 +0100

    Create 33_get_users_objects.sql

---
+++ sql/33_get_users_objects.sql
@@ -0,0 +1,25 @@
+select * from
+(
+    select owner, object_type ,count(*) as object_count from dba_objects 
+    where owner not in ('ANONYMOUS','APEX_180200','APEX_INSTANCE_ADMIN_USER','APEX_PUBLIC_USER','APPQOSSYS','AUDSYS','CTXSYS','DBSFWUSER','DBSNMP','DIP','DVF','DVSYS','FLOWS_FILES','GGSYS','GSMADMIN_INTERNAL','GSMCATUSER','GSMUSER','LBACSYS','MDDATA','MDSYS','OJVMSYS','OLAPSYS','ORACLE_OCM','ORDDATA','ORDPLUGINS','ORDSYS','OUTLN','REMOTE_SCHEDULER_AGENT','SI_INFORMTN_SCHEMA','SYS','SYS$UMF','SYSBACKUP','SYSDG','SYSKM','SYSRAC','SYSTEM','WMSYS','XDB','XS$NULL')
+    group by owner, object_type order by owner, object_type
+)
+  pivot
+  (
+     max(object_count)
+     for object_type in (
+             'TABLE',
+             'VIEW',
+             'INDEX',
+             'FUNCTION',
+             'LOB',
+             'PACKAGE',
+             'PROCEDURE',
+             'TRIGGER',
+             'SYNONYM'
+        )
+  )
+  order by owner
+;
+
+
