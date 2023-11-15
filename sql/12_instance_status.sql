98e8900644c469c776077461d9704da111c7e673
Yacine31 <yacine.oumghar@gmail.com>
2023-11-15 12:25:38 +0100

    Update 12_instance_status.sql

--- sql/12_instance_status.sql
+++ sql/12_instance_status.sql
@@ -1,9 +1,10 @@
 prompt <h2>Database/Instance Status</h2>
+alter session set nls_date_format='YYYY/MM/DD HH24:MI:SS';
 select 
  inst_id,
  instance_name,
  host_name, 
-to_char(startup_time ,'DD/MM/YYYY') startup_time, 
+to_char(startup_time ,'DD/MM/YYYY HH24:MI:SS') startup_time, 
  status,
 -- VERSION_FULL,
 -- EDITION,
@@ -16,7 +17,7 @@ FROM  gv$instance;
 SELECT 
  inst_id,
  name,
- to_char(CREATED ,'DD/MM/YYYY') CREATED ,
+ to_char(CREATED ,'DD/MM/YYYY HH24:MI:SS') CREATED ,
  open_mode,
  DATABASE_ROLE,
  log_mode,
@@ -25,4 +26,4 @@ SELECT
 
 select * from v$version;
 
-exit
+
