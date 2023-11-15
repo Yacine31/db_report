8245bc6dab3d0e2f0db7954b1b4366866db0f52a
Yacine31 <yacine.oumghar@gmail.com>
2023-11-15 12:20:36 +0100

    Update 15_check_supplemntal_logging.sql

--- sql/15_check_supplemntal_logging.sql
+++ sql/15_check_supplemntal_logging.sql
@@ -1,18 +1,19 @@
 prompt <h2>Database supplemental logging :</h2>
 
-col data_min for a10
-col data_pk for a10
-col data_ui for a10
-col data_fk for a10
-col data_all for a10
-col data_pl for a10
-
+col data_mining for a15
+col data_pkey for a15
+col data_uniq_key for a15
+col data_fk for a15
+col data_all for a15
+col data_pl for a15
+col force_logging for a15
 SELECT 
-	supplemental_log_data_min data_min, 
-	supplemental_log_data_pk data_pk, 
-	supplemental_log_data_ui data_ui, 
+	force_logging,
+	supplemental_log_data_min data_mining, 
+	supplemental_log_data_pk data_pkey, 
+	supplemental_log_data_ui data_uniq_key, 
 	supplemental_log_data_fk data_fk, 
 	supplemental_log_data_all data_all, 
 	supplemental_log_data_pl data_pl
 FROM v$database;
-exit
+
