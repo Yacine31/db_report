d53e598c416294c665437c549f8ccab5d764bb7d
Yacine31 <yacine.oumghar@gmail.com>
2023-11-15 12:15:55 +0100

    Update 19_memory_resize_operations.sql

--- sql/19_memory_resize_operations.sql
+++ sql/19_memory_resize_operations.sql
@@ -1,4 +1,5 @@
 prompt <h2>Les 50 dernières opérations de Resize de la mémoire :</h2>
+alter session set nls_date_format='YYYY/MM/DD HH24:MI:SS';
 SELECT *
   FROM gv$memory_resize_ops
   where rownum <=50
@@ -6,4 +7,3 @@ SELECT *
        inst_id,
        start_time DESC,
        component;
-exit 
