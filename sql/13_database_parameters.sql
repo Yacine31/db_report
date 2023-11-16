d8675aaf1d79500a0b7d234bebda866f402e2f3b
Yacine31 <yacine.oumghar@gmail.com>
2023-11-16 02:10:28 +0100

    Update 13_database_parameters.sql

--- sql/13_database_parameters.sql
+++ sql/13_database_parameters.sql
@@ -1,2 +1,2 @@
 prompt <h2>Paramèters de la base de données : </h2>
-select NAME, DISPLAY_VALUE from v$parameter where ISDEFAULT='FALSE' order by name;
+select NAME, DISPLAY_VALUE, DESCRIPTION, UPDATE_COMMENT from v$parameter where ISDEFAULT='FALSE' order by name;
