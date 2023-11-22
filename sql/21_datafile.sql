5107c213e43d3f7c8593dadadacbd535b79275bf
Yacine31 <yacine.oumghar@gmail.com>
2023-11-22 15:52:08 +0100

    Update 21_datafile.sql

--- sql/21_datafile.sql
+++ sql/21_datafile.sql
@@ -1,17 +1,23 @@
 prompt <h2>Détail des datafiles : </h2>
 
-COL file_name HEAD "Datafile"
 COL file_id HEAD "File ID"
-COL TABLESPACE_NAME FORMAT A20 HEAD "Tablespace"
+COL file_name HEAD "Datafile"
+COL tablespace_name FORMAT A20 HEAD "Tablespace"
+
+COL bytes FORMAT 99999999.00 HEAD "Bytes MB"
+COL maxbytes FORMAT 99999999.00 HEAD "MaxBytes MB"
+COL Pct_Used FORMAT 999.00 HEAD "% Used"
+
+
 COL autoextensible FORMAT A15 HEAD "Auto Extensible"
 COL status head "Statut"
 COL online_status format a15 head "Online Statut"
 
 SELECT  
        file_id, file_name, tablespace_name, 
-       round(bytes/1024/1024,0) "Bytes Mo",  
-       round(maxbytes/1024/1024,0) "MaxBytes Mo", 
-       round(100*bytes/maxbytes) "% Used",
+       round(bytes/1024/1024,0) ,  
+       round(maxbytes/1024/1024,0) , 
+       round(100*bytes/maxbytes) Pct_Used,
        online_status,
        status, autoextensible
   FROM dba_data_files 
