34b262d35684dafaf2b579b81e1a5d6553622566
Yacine31 <yacine.oumghar@gmail.com>
2023-11-22 15:57:40 +0100

    Update 21_datafile.sql

--- sql/21_datafile.sql
+++ sql/21_datafile.sql
@@ -15,8 +15,8 @@ COL online_status format a15 head "Online Statut"
 
 SELECT  
        file_id, file_name, tablespace_name, 
-       round(bytes/1024/1024,0) ,  
-       round(maxbytes/1024/1024,0) , 
+       round(bytes/1024/1024,0) bytes,  
+       round(maxbytes/1024/1024,0) maxbytes, 
        round(100*bytes/maxbytes) Pct_Used,
        online_status,
        status, autoextensible
