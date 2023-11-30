481d358600629867e66d794239e882d2d2562111
Yacine31 <yacine.oumghar@gmail.com>
2023-11-30 01:43:08 +0100

    Update 22_online_log.sql

--- sql/22_online_log.sql
+++ sql/22_online_log.sql
@@ -1,20 +1,49 @@
 prompt <h2>Fichiers de journalisation (Redolog) :</h2>
 alter session set nls_date_format='YYYY/MM/DD HH24:MI:SS';
-SELECT /*+  NO_MERGE  */ 
-     *
-  FROM v$log
- ORDER BY 1, 2, 3, 4;
 
-prompt <h2>Multiplexage des fichiers de journalisation (Redolog)</h2>
+SELECT
+    *
+FROM
+    v$log
+ORDER BY
+    group#,
+    thread#,
+    sequence#;
+
+prompt <h2>Multiplexage des fichiers de journalisation (Redolog et standbylog)</h2>
+
 COL MEMBER FORMAT A90 WRAPPED
 BREAK ON GROUP# SKIP 1 ON THREAD# ON SEQUENCE# ON TAILLE_MIB ON "STATUS(ARCHIVED)"
-SELECT 'OnlineLog' T, G.GROUP#, G.THREAD#, G.SEQUENCE#, G.BYTES/1024/1024 TAILLE_MIB, G.STATUS||'('||G.ARCHIVED||')' "STATUS(ARCHIVED)", F.MEMBER
-FROM V$LOG G, V$LOGFILE F
-WHERE G.GROUP#=F.GROUP#
+SELECT
+    'OnlineLog'           t,
+    g.group#,
+    g.thread#,
+    g.sequence#,
+    g.bytes / 1024 / 1024 taille_mib,
+    g.status || '(' || g.archived || ')' "STATUS(ARCHIVED)",
+    f.member
+FROM
+    v$log     g,
+    v$logfile f
+WHERE
+    g.group# = f.group#
 UNION ALL
-SELECT 'StandbyLog',G.GROUP#, G.THREAD#, G.SEQUENCE#, G.BYTES/1024/1024 TAILLE_MIB, G.STATUS||'('||G.ARCHIVED||')' "STATUS(ARCHIVED)", F.MEMBER
-FROM V$STANDBY_LOG G, V$LOGFILE F
-WHERE G.GROUP#=F.GROUP#
-ORDER BY 1,3,4,2;
-
+SELECT
+    'StandbyLog',
+    g.group#,
+    g.thread#,
+    g.sequence#,
+    g.bytes / 1024 / 1024 taille_mib,
+    g.status || '(' || g.archived || ')' "STATUS(ARCHIVED)",
+    f.member
+FROM
+    v$standby_log g,
+    v$logfile     f
+WHERE
+    g.group# = f.group#
+ORDER BY
+    1,
+    3,
+    4,
+    2;
 
