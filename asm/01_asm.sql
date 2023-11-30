be146b5f80377684e2fe74481db61ada40f0fb04
Yacine31 <yacine.oumghar@gmail.com>
2023-11-30 01:27:18 +0100

    Update 01_asm.sql

--- asm/01_asm.sql
+++ asm/01_asm.sql
@@ -1,23 +1,53 @@
 PRO <h2>Configuratiom ASM</h2>
 
-select * from GV_$ASM_DISKGROUP;
+SELECT
+    dg.name,
+    dg.state,
+    dg.type,
+    dg.total_mb,
+    dg.free_mb,
+    dg.usable_file_mb,
+    compatibility,
+    dg.database_compatibility
+FROM
+    v$asm_diskgroup dg;
 
+-- Viewing disks in disk groups with V$ASM_DISK
 
-select 
-    a.inst_id,
-    a.group_number,
-    a.disk_number,
-    a.header_status,
-    a.mode_status,
-    a.state,
-    a.redundancy,
+SELECT
+    dg.name "Disk Grp Name",
+    a.name  "Name",
+    a.failgroup,
+    a.path,
     a.os_mb,
     a.total_mb,
     a.free_mb,
     a.cold_used_mb,
-    a.name,
-    a.failgroup,
-    a.path,
-    a.create_date
-from GV_$ASM_DISK a;
+    a.header_status,
+    a.mode_status,
+    a.state,
+    a.redundancy,
+    to_char(a.create_date, 'DD/MM/YYYY HH24:MI')
+FROM
+    v$asm_disk      a,
+    v$asm_diskgroup dg
+WHERE
+    a.group_number = dg.group_number
+ORDER BY
+    dg.name,
+    a.name
+;
 
+-- Viewing disk group clients with V$ASM_CLIENT
+
+SELECT
+    dg.name            AS diskgroup,
+    c.instance_name    AS instance,
+    db_name            AS dbname,
+    software_version   AS software,
+    compatible_version AS compatible
+FROM
+    v$asm_diskgroup dg,
+    v$asm_client    c
+WHERE
+    dg.group_number = c.group_number;
