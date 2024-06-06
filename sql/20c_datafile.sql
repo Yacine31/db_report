97b27fd03bc66ceeae8742216efb73cafbac352f
Yacine31 <yacine.oumghar@gmail.com>
2024-06-06 06:51:40 +0200

    Update 20c_datafile.sql

--- sql/20c_datafile.sql
+++ sql/20c_datafile.sql
@@ -36,6 +36,7 @@ cdb_files AS (
         JOIN cdb_pdbs p ON d.con_id = p.pdb_id
     WHERE 
         (SELECT cdb FROM v$database) = 'YES'
+    ORDER BY p.pdb_id, d.tablespace_name, d.file_name
 ),
 -- Sous-requête pour les fichiers de données dans une non-CDB
 non_cdb_files AS (
@@ -71,9 +72,9 @@ non_cdb_files AS (
         JOIN dba_data_files d ON a.file_id = d.file_id
     WHERE 
         (SELECT cdb FROM v$database) = 'NO'
+    ORDER BY p.pdb_id, d.tablespace_name, d.file_name
 )
 -- Requête finale combinant les résultats des sous-requêtes
 SELECT * FROM cdb_files
 UNION ALL
 SELECT * FROM non_cdb_files;
-ORDER BY p.pdb_id, d.tablespace_name, d.file_name
