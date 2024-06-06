f49a4fe72b928a58df5cc7fcc078bf8acc5d2499
Yacine31 <yacine.oumghar@gmail.com>
2024-06-06 02:54:43 +0200

    Update 01_show_pdbs.sql

--- sql/cdb/01_show_pdbs.sql
+++ sql/cdb/01_show_pdbs.sql
@@ -1,3 +1,16 @@
 prompt <h2>Liste des PLuggables Databases</h2>
 
-show pdbs;
+SELECT
+    p.con_id,
+    p.dbid,
+    p.name,
+    p.open_mode,
+    p.restricted,
+    p.open_time,
+    p.application_root,
+    p.application_pdb,
+    p.application_seed
+FROM
+    v$pdbs p
+ORDER BY
+    con_id;
