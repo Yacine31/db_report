02ddef481d88d5f03de6fcd43fd9c66fb7dd6d37
Yacine31 <yacine.oumghar@gmail.com>
2024-06-18 01:25:15 +0200

    Update 09_os_info.sh

--- sh/09_os_info.sh
+++ sh/09_os_info.sh
@@ -8,11 +8,11 @@ echo "</pre>"
 
 echo "<h2>Listeners en cours d'exécution :</h2>"
 echo "<pre>"
-ps -ef | grep tnslsnr| grep -v grep 
+ps -ef | grep tnslsnr | grep -v grep 
 echo "</pre>"
 
 echo "<h2>Statut des listeners :</h2>"
-ps -ef | grep tnslsnr | egrep -i "LISTENER|${ORACLE_SID}" | grep -v grep | while read l
+ps -ef | grep tnslsnr | egrep -i "LISTENER_${ORACLE_SID}" | grep -v grep | while read l
 do
 	# Récupérer le chemin ORACLE_HOME à partir de la sortie de ps -ef
 	# ---- La commande grep -o ne fonctionne pas sur AIX, on la remplace par perl -lne
