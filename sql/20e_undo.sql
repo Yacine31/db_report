920f6bca1d31eadb86473a2b8d2c74093a3c6f70
Yacine31 <yacine.oumghar@gmail.com>
2025-04-17 01:21:18 +0200

    update undo

--- sql/20e_undo.sql
+++ sql/20e_undo.sql
@@ -15,29 +15,4 @@ JOIN
      FROM dba_free_space
      GROUP BY file_id) b
 ON a.file_id = b.file_id
-WHERE a.tablespace_name like 'UNDO%';  -- ou le nom réel de ton UNDO
-
-prompt <h3>Durée de rétention UNDO : </h3>
-SELECT
-    retention,
-    tuned_undoretention,
-    undoblks,
-    expiredblks,
-    unexpiredblks,
-    tuned_undoretention
-FROM
-    v$undostat
-ORDER BY
-    begin_time DESC FETCH FIRST 10 ROWS ONLY;
-
-prompt <h3>Occupation de l’espace dans le UNDO par session : </h3>
-SELECT
-    s.sid,
-    s.serial#,
-    u.name AS username,
-    t.used_urec,
-    t.used_ublk
-FROM
-    v$transaction t
-JOIN v$session s ON t.ses_addr = s.saddr
-JOIN all_users u ON s.user# = u.user_id;
+WHERE a.tablespace_name like 'UNDO%';
