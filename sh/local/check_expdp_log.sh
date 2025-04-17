e58a87826d2a15fd17685f575b71d0d0d93d1631
Yacine31 <yacine.oumghar@gmail.com>
2025-04-17 20:15:03 +0200

    Adaptation de find dans local.sh à l'env de test YOU

--- sh/local/check_expdp_log.sh
+++ sh/local/check_expdp_log.sh
@@ -8,7 +8,7 @@ echo "<h2>Vérification des logs des exports Datapump</h2>"
 
 #  vérifier si une erreur ORA- est pésente dans les fichiers logs 
 echo "<h3>Vérification de la présence d'erreurs dans les logs :</h3>"
-RESULT=$(find "${EXPDP_DIR}" -iname "export*${CURRENT_DATE}*log" -exec grep -H "ORA-" "{}" \;)
+RESULT=$(find "${EXPDP_DIR}" -iname "export_*.log" -exec grep -H "ORA-" "{}" \;)
 if [ -z "$RESULT" ]; then
     echo "<pre>Aucune erreur ORA- détectée dans les fichiers logs du mois ${CURRENT_DATE}.<pre>"
 else
@@ -18,5 +18,5 @@ fi
 # afficher les dernières lignes des fichiers log pour voir les les exports se sont bien déroulés
 echo "<h3>Vérification des dernières lignes dans les logs :</h3>"
 echo "<pre>"
-find "${EXPDP_DIR}" -iname "export*${CURRENT_DATE}*log" -exec bash -c 'echo "--- {} ---"; tail -3 "{}"' \;
+find "${EXPDP_DIR}" -iname "export_*.log" -exec bash -c 'echo "--- {} ---"; tail -3 "{}"' \;
 echo "</pre>"
