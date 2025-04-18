d5b1cc6ee15cb166f2d1f3500452da7bcbbe19c1
Yacine31 <yacine.oumghar@gmail.com>
2025-04-18 20:05:19 +0200

    mise à jour de certains scripts SQL

--- sql/14_nls_database_parameters.sql
+++ sql/14_nls_database_parameters.sql
@@ -1,8 +1,4 @@
 prompt <h2>Paramètres NLS au niveau de la base de données :</h2>
 
-SELECT
-    *
-FROM
-    NLS_DATABASE_PARAMETERS
-ORDER BY
-    PARAMETER;
+-- SELECT * FROM NLS_DATABASE_PARAMETERS ORDER BY PARAMETER;
+select * from gv$nls_parameters order by PARAMETER ;
---
+++ sql/35b_cursors_count.sql
@@ -0,0 +1,8 @@
+prompt <h2>Curseurs par Session (sans les users SYS, SYSTEM, SYSMAN)</h2>
+
+select c.sid, s.username, count(*) open_cursors
+    from gv$open_cursor c, gv$session s
+    where s.sid = c.sid
+    and s.username is not null and s.username not like 'SYS%'
+    group by c.sid, s.username
+;
