6f89b71f0d8752b5535abcc45f89f172c94061ed
Yacine31 <yacine.oumghar@gmail.com>
2025-04-28 15:32:43 +0200

    correction alertlog HH24 au lieu de HH

--- sql/25_last_alertlog_errors.sql
+++ sql/25_last_alertlog_errors.sql
@@ -1,7 +1,7 @@
 prompt <h2>Les dernières erreurs de la base de données (Les 30 derniers jours et les 50 dernières lignes)</h2>
 
 SELECT
-    TO_CHAR(ORIGINATING_TIMESTAMP, 'DD-MM-YYYY HH:MM:SS') "Date",
+    TO_CHAR(ORIGINATING_TIMESTAMP, 'DD-MM-YYYY HH24:MM:SS') "Date",
     MESSAGE_TEXT                                          "Message"
 FROM
     X$DBGALERTEXT
