63e0f953d976df7d7be13d05e0f9dc99982f4a1c
Yacine31 <yacine.oumghar@gmail.com>
2025-12-12 23:09:07 +0100

    - suppression des commandes en double niveau OS - split des commandes OS et des commandes Oracle pmon et lsnrt

---
+++ sh/08_oracle_process.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Importe les fonctions utilitaires
+source "$(dirname "$0")/utils.sh"
+
+# --- Script principal ---
+
+os_type=$(uname -s)
+
+print_h2 "Bases de données en cours d'exécution"
+run_and_print "ps -ef | grep pmon | grep -v grep"
+
+print_h2 "Listeners en cours d'exécution"
+run_and_print "ps -ef | grep tnslsnr | grep -v grep"
+
+print_h2 "Statut du listener : ${listener_name}"
+# Boucle pour le statut du listener, car elle est plus complexe
+ps -ef | grep tnslsnr | egrep -i " LISTENER |${ORACLE_SID}" | grep -v grep | while read -r l; do
+  binary_path=$(echo "$l" | perl -lne 'print $1 if /(\S*tnslsnr\S*)/' | sed 's#/bin/tnslsnr##')
+  listener_name=$(echo "$l" | perl -lne 'print $1 if /\btnslsnr\s+(\S+)/' | sed 's/tnslsnr //')
+  
+  if [ -n "$binary_path" ] && [ -n "$listener_name" ]; then
+    export TNS_ADMIN="$binary_path/network/admin"
+    lsnrctl_command="$binary_path/bin/lsnrctl status $listener_name"
+    # echo "<b>Listener: ${listener_name}</b>"
+    run_and_print "$lsnrctl_command"
+  fi
+done
+
+print_h2 "Limites de l'utilisateur (ulimit -a)"
+run_and_print "ulimit -a | sort"
+
+
+
--- sh/09_os_info.sh
+++ sh/09_os_info.sh
@@ -1,38 +1,8 @@
 #!/bin/bash
-
 # Importe les fonctions utilitaires
 source "$(dirname "$0")/utils.sh"
 
 # --- Script principal ---
-
-#!/bin/bash
-
-# Importe les fonctions utilitaires
-source "$(dirname "$0")/utils.sh"
-
-# --- Script principal ---
-
-os_type=$(uname -s)
-
-print_h2 "Bases de données en cours d'exécution"
-run_and_print "ps -ef | grep pmon | grep -v grep"
-
-print_h2 "Listeners en cours d'exécution"
-run_and_print "ps -ef | grep tnslsnr | grep -v grep"
-
-print_h2 "Statut du listener : ${listener_name}"
-# Boucle pour le statut du listener, car elle est plus complexe
-ps -ef | grep tnslsnr | egrep -i " LISTENER |${ORACLE_SID}" | grep -v grep | while read -r l; do
-  binary_path=$(echo "$l" | perl -lne 'print $1 if /(\S*tnslsnr\S*)/' | sed 's#/bin/tnslsnr##')
-  listener_name=$(echo "$l" | perl -lne 'print $1 if /\btnslsnr\s+(\S+)/' | sed 's/tnslsnr //')
-  
-  if [ -n "$binary_path" ] && [ -n "$listener_name" ]; then
-    export TNS_ADMIN="$binary_path/network/admin"
-    lsnrctl_command="$binary_path/bin/lsnrctl status $listener_name"
-    # echo "<b>Listener: ${listener_name}</b>"
-    run_and_print "$lsnrctl_command"
-  fi
-done
 
 print_h2 "Uptime"
 run_and_print "uptime"
@@ -41,12 +11,8 @@ case "$os_type" in
     AIX)
         print_h2 "Espace disque (lsfs)"
         run_and_print "lsfs"
-        print_h2 "Espace disque (lsfs)"
-        run_and_print "lsfs"
         ;;
     Linux)
-        print_h2 "Contenu du fichier /etc/fstab"
-        run_and_print "cat /etc/fstab | egrep -v '^#|^$'"
         print_h2 "Contenu du fichier /etc/fstab"
         run_and_print "cat /etc/fstab | egrep -v '^#|^$'"
         ;;
@@ -54,24 +20,13 @@ esac
 
 print_h2 "Contenu du contab du compte oracle"
 run_and_print "crontab -l"
-print_h2 "Contenu du contab du compte oracle"
-run_and_print "crontab -l"
-
-print_h2 "Limites de l'utilisateur (ulimit -a)"
-run_and_print "ulimit -a | sort"
-print_h2 "Limites de l'utilisateur (ulimit -a)"
-run_and_print "ulimit -a | sort"
 
 case "$os_type" in
     AIX)
         print_h2 "Espace disque (df -g)"
         run_and_print "df -g"
-        print_h2 "Espace disque (df -g)"
-        run_and_print "df -g"
         ;;
     Linux)
-        print_h2 "Espace disque (df -h)"
-        run_and_print "df -h"
         print_h2 "Espace disque (df -h)"
         run_and_print "df -h"
         ;;
@@ -140,8 +95,6 @@ esac
 
 case "$os_type" in
     AIX)
-        print_h2 "Configuration système (prtconf)"
-        run_and_print "prtconf"
         print_h2 "Configuration système (prtconf)"
         run_and_print "prtconf"
         ;;
