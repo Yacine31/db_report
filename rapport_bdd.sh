be94f44db8e865c0b32e9066fa0845c0bc5910b1
Yacine31 <yacine.oumghar@gmail.com>
2025-12-16 00:24:00 +0100

    feat: update h2 title

--- rapport_bdd.sh
+++ rapport_bdd.sh
@@ -148,7 +148,7 @@ for database_sid in ${database_sids}; do
     echo "<h1>Rapport de base de données "${ORACLE_SID}" sur le serveur "${hostname}"</h1>"
     echo "<h3>Date : "${current_date}"</h3>"
     echo "<h3>Hostname : "${hostname}"</h3>"
-    echo "<h2>Base de données : "${ORACLE_SID}"</h2>"
+    echo "<h2>Database : "${ORACLE_SID}"</h2>"
     echo "<br><br>"
    } >> "${html_report_file}"
 
